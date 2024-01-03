89c3cb40717902846f94dbd6ee330fb9703b0640
wnsgud1997 <wnsgud1997@gmail.com>
2024-01-03 17:42:38 +0900

    2024.01.03 코딩테스트 예제문

--- CodingTest/src/step1/Main1_8.java
+++ CodingTest/src/step1/Main1_8.java
@@ -6,22 +6,18 @@ public class Main1_8 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int C = sc.nextInt();
-
-		if(A >= 2 && A <= 10000 && B >= 2 && B <= 10000 && C >= 2 && C <= 10000) {
-			int rs1 = (A+B)%C;
-			int rs2 = ((A%C) + (B%C))%C;
-			int rs3 = (A*B)%C;
-			int rs4 = ((A%C) * (B%C))%C;
-
-			System.out.println(rs1);
-			System.out.println(rs2);
-			System.out.println(rs3);
-			System.out.println(rs4);
-		}else {
-			System.out.println("재입력");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		int rs = num1 * num2;
+		
+		for(int i=3 ; i <= 6 ; i++) {
+			
+			int rs3= rs% 10;
+			rs/=10;
+			
+			System.out.println(+i);
 		}
+		
 	}
 }

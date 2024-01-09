2022fb657b3fb50efa753e64eb2ed1f41f374b39
wnsgud1997 <wnsgud1997@gmail.com>
2024-01-09 17:50:22 +0900

    2024.01.09 Java 예제문

---
+++ Ch06/src/sub2/ThrowsTest.java
@@ -0,0 +1,38 @@
+package sub2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/*
+ *  날짜 : 2024.01.08
+ *  이름 : 김준형
+ *  내용 : Java 예외처리 실습하기
+ */
+public class ThrowsTest {
+	public static void main(String[] args) {
+		try {
+		method1(10);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("프로그램 정상 종료...");
+		
+	}
+	public static void method1(int n1) throws Exception { //method1 호출한 곳으로 예외 던지기
+		
+		method2(n1);
+		
+	}
+	public static void method2(int n2) throws Exception { //method2 호출한 곳으로 예외 던지기
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자입력 : " );
+		int input = sc.nextInt();
+		
+		int result = n2 / input;
+		
+		System.out.println("result : " +result);
+	}
+	
+}

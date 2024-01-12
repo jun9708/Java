1724b6f3591b3fc5711e617d5252ad3ee3157d50
wnsgud1997 <wnsgud1997@gmail.com>
2024-01-12 17:48:41 +0900

    2024.01.12 Java 연습문제4

---
+++ Exam/src/test4/Test08.java
@@ -0,0 +1,49 @@
+package test4;
+/*
+ * 	날짜 : 2024.01.12
+ * 	이름 : 김준형
+ * 	내용 : 객체 리스트 연습문제
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Member {
+	String id;
+	String name;
+	int age;
+	public Member(String id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return id + ", " + name + ", " + age;
+	}
+
+
+
+
+
+
+}
+
+public class Test08 {
+	public static void main(String[] args) {
+		Member m1 = new Member("hong", "홀길동", 30);
+		Member m2 = new Member("lee", "이순신", 40);
+		Member m3 = new Member("kim", "김유신", 50);
+		
+		
+		List<Member> memberList = new ArrayList<>();
+		memberList.add(m1);
+		memberList.add(m2);
+		memberList.add(m3);
+		
+		for(Member m : memberList) {
+			System.out.println(m);
+		}
+	}
+}
